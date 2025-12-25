4c08ba7cda65e343819cb80a4563bd1f7eaee421
PRINCE TECH <majidiabduli2003@gmail.com>
2025-12-25 23:59:46 +0300

    Update clearsession.js

--- commands/clearsession.js
+++ commands/clearsession.js
@@ -8,8 +8,8 @@ const channelInfo = {
         forwardingScore: 999,
         isForwarded: true,
         forwardedNewsletterMessageInfo: {
-            newsletterJid: '120363161513685998@newsletter',
-            newsletterName: 'KnightBot MD',
+            newsletterJid: '120363422824898123@newsletter',
+            newsletterName: 'PRINCE XMD',
             serverMessageId: -1
         }
     }
